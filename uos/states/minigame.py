6b59fd714fd6a29f99fdc77a955b44a2532e888e
R3ngad33 <35588970+R3ngad33@users.noreply.github.com>
2018-10-03 06:07:08 -0400

    Major groundwork for minigame added
    -added configuration variables
    -added location array builder
    -junk generater/filler
    -parse words and bracket sets for highligher
    -multipurpose likeness tester
    -get words based on likeness to secret word

--- uos/states/minigame.py
+++ uos/states/minigame.py
@@ -1,5 +1,6 @@
 import pygame
-from random import randint
+import math
+from random import randint,random
 from ..uos import UOS
 from ..writer import Writer
 
@@ -18,11 +19,19 @@ class MinigameBase(UOS.State):
         self.writer.add_output(pygame.Rect(188, h * 4, 186, 308))
         # body3
         self.writer.add_input(pygame.Rect(364, h, 140, 342), True, 0, True)
+        # configuration variables
         self.header = 'temp header'
+        self.hex_seed = randint(4096, 65535)
         self.attempts = attempts
+        self.difficulty = 5
+        self.line_length = 12
+        self.lines = 32
+        self.characters = self.line_length * self.lines
+        self.secret_word = 'APPLE'
+        self.words = []
+        self.places = []
+        self.display = ''
         self.select = 0
-        self.hex_seed = randint(4096,65535)
-        self.password = 'APPLE'
 
     def call_back(self):
         UOS.State.flip_state = self._state.track
@@ -30,13 +39,6 @@ class MinigameBase(UOS.State):
     def call_selection(self):
         pass
 
-    def create_highlighter(self):
-        self.selection = []
-        for string in self.strings:
-            if string:
-                self.selection.append(self.render_text(string))
-            else:
-                self.selection.append(None)
 
     def attempts_remaining(self, count):
         debug_text = 'Attempts Remaining:'
@@ -52,27 +54,166 @@ class MinigameBase(UOS.State):
             hex_num += 12
             x += 1
 
+    # randomizes world locations, returns 2d array with paired index and word
+    def find_places(self,words,difficulty,characters):
+        places = []
+        placed = False
+        blacklist = []
+        for x in range(len(words)):
+            placed = False
+            while not placed and len(blacklist) < characters * 0.8:
+                place = math.floor(random.random() * (characters - difficulty))
+                # check blacklist array to make sure there isn't a word conflict
+                if place in blacklist < 0:
+                    places.append([place,words[x]])
+                    places = True
+                    # add used spaces to blacklist array
+                    j = place - difficulty
+                    while j <= place + difficulty:
+                        blacklist.append(j)
+                        j += 1
+        return places.sort()  # this might need tweaking
+
+    # creates the full grid by filling remaining space with junk
+    def generate_display(self,places,difficulty,characters):
+        places = places[:]
+        display = ''
+        for x in range(len(characters)):
+            if len(places) > 0 and x == places[0][0]:
+                display += places[0][1]
+                x += difficulty
+            else:
+                display += self.random_character()
+                x += 1
+        return display
+
+    # gives random junk character
+    def random_character(self):
+        fillers = ['(',')',
+                   '<','>',
+                   '{','}',
+                   '[',']',
+                   '=',
+                   '_',
+                   '-',
+                   '.',
+                   '/',
+                   '$',
+                   '@',
+                   ';',
+                   ':',
+                   '%',
+                   '^',
+                   '&',
+                   '|',
+                   ',',
+                   '*',
+                   '\'']
+        return str(fillers[randint(0,len(fillers))])
+
+    # takes the entire code string and breaks it into lines
+    def split_into_lines(self,display,lines,line_length):
+        screen = []
+        for x in range(len(lines)):
+            screen.append(display[0:line_length])
+            display = display[line_length]
+        return screen
+
+    # highlight words UNFINISHED
+    def highlight_word(self,places,difficulty):
+        for x in range(len(places)):
+            position = int(places[x][0])
+            j = position
+            while j < position + difficulty:
+                # todo make index of words
+                j += 1
+
+    # highlight bracket sets
+    def highlight_bracket_set(self,lines):
+        openers = ['(','[','{','<']
+        closers = [')',']','}','>']
+        sets = []
+        index = 0
+        # iterates over each line to find sets
+        for x in range(len(lines)):
+            for j in range(len(lines[x])):
+                letter = lines[x][j]
+                if letter in openers >=0:
+                    br_index = letter in openers
+                    queue = lines[x:j]  # not sure if this is right
+                    if closers[br_index] in queue > 0:
+                        end_index = closers[br_index] in queue
+                        snippet = queue[0:end_index + 1]
+                        chars = set('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+                        if any((c in chars) for c in snippet):  # supposed to check for letters
+                            index +=1
+                        else:
+                            sets.append([index, index + end_index, snippet])
+
+    # handle selection highlighting
+    def change_selection(self):
+        select = self.select
+        # todo
+        # if a letter in a word is selected, highlights and selects the whole word
+        # print current selection to the input line without committing it to writer queue
+        # if at the edge of a column move to next column
+
+    # runs bracket functions
+    def hack(self):
+        roll = randint(0,4)
+        if roll == 4:
+            self.reset_tries()
+        else:
+            self.remove_dud()
+
+    # sets attempts back to 4
+    def reset_tries(self):
+        self.attempts = 4
+
+    # removes dud
     def remove_dud(self):
         UOS.sounds.play('dud')
 
-    def test_likeness(self, password, attempt):
-        length = len(password)
-        matches = 0
-        for x in range(length):
-            if password[x] == attempt[x]:
-                matches += 1
-        if matches == length:
-            UOS.sounds.play('good')
-            # do stuff here
+    # gets character likeness
+    def get_likeness(self, word, secret_word, visible=True):
+        likeness = 0
+        for x in range(len(secret_word)):
+            if word[x] == secret_word[x]:
+                likeness += 1
+        if visible:
+            if likeness == len(secret_word):
+                UOS.sounds.play('good')
+                # todo pass valid logon
+            else:
+                UOS.sounds.play('bad')
+                self.writer.add(3, '> Entry denied.')
+                self.writer.add(3, '> Likeness=' + likeness)
+                self.attempts -= 1
+                # todo if 0 attempts lock
         else:
-            UOS.sounds.play('bad')
-            self.writer.add(3, '> Entry denied.')
-            self.writer.add(3, '> Likeness=' + matches)
-            # remove attempt
+            return likeness
+
+    # make sure words are random, but at least similar to secret word
+    def get_words(self,secret_word):
+        word = ''
+        likeliness = 0
+        wordset = []  # todo access to word list based on length
+        words = [secret_word]
+        for x in range(15):
+            found = False
+            count = 0
+            while not found:
+                # get random word from wordset
+                word = wordset[randint(0,len(wordset))]
+                # test likeness of word to secret word
+                likeliness = self.get_likeness(word,secret_word,False)
+                if word not in words and likeliness < randint(1,3) or count > 100:  # important to avoid infinite loop
+                    words.append(word)
+                    found = True
+        return words
 
 
     def display_string(self):
-        self.create_highlighter()
         self.writer.add(0, self.header)
         self.writer.add(0, "Password Required")
         self.writer.add(0, self.attempts_remaining(self.attempts))
@@ -93,18 +234,18 @@ class MinigameBase(UOS.State):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     UOS.sounds.play('scroll')
-                    self.select += 11
+                    self.select += 12
                     self.select %= len(self.selection)
                     while self.selection[self.select] is None:
-                        self.select += 11
+                        self.select += 12
                         self.select %= len(self.selection)
 
                 elif event.key == pygame.K_UP:
                     UOS.sounds.play('scroll')
-                    self.select -= 11
+                    self.select -= 12
                     self.select %= len(self.selection)
                     while self.selection[self.select] is None:
-                        self.select -= 11
+                        self.select -= 12
                         self.select %= len(self.selection)
 
                 elif event.key == pygame.K_LEFT:
@@ -124,7 +265,7 @@ class MinigameBase(UOS.State):
 
                 elif event.key == pygame.K_RETURN:
                     UOS.sounds.play('attempt')
-                    self.test_likeness(self.password,self.selection[self.select])  # todo get selected word
+                    # todo get selected word
 
     def render(self, surface):
         surface.fill((0,0,0))
